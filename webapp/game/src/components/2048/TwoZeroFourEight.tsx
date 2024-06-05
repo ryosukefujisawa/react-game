c9f1184db0f2f32765a1f30dd914a3680edb688e
ryosuke fujisawa <ryosuke.fujisawa@goodsun-hd>
2024-06-05 11:39:06 +0900

    Move React-Game

---
+++ webapp/game/src/components/2048/TwoZeroFourEight.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Board from "./Board";
+import GameOver from "../GameOver";
+import '../css/2048.css';
+
+
+interface TwoZeroFourEightProps {
+    setGame: React.Dispatch<React.SetStateAction<string>>;
+}
+
+
+const TwoZeroFourEight = (props: TwoZeroFourEightProps) => {
+        const [gameover, setGameOver] = useState<boolean>(false);
+
+        return (
+            <>
+                <a onClick={() => props.setGame("menu")}><h1>2048</h1></a>
+
+                { !gameover ?
+                    <Board /> :
+                    // <GameOver setGameOver={setGameOver} setGame={props.setGame} />
+                    <h1>GameOver</h1>
+                }
+            </>
+        );
+};
+
+export default TwoZeroFourEight;
