b493c2960e538d5df601264b2d91f3a041af8005
ryosuke fujisawa <ryosuke.fujisawa@goodsun-hd>
2024-06-06 11:44:48 +0900

    URL

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -21,7 +21,7 @@ const Game = () => {
     const fetchScores = async () => {
       try {
         /* サーバーにHTTP GETリクエストを送信してスコアのリストを取得 */
-        const response = await axios.get<Score[]>('/scores');
+        const response = await axios.get<Score[]>('https://localhost/3001/scores');
         setScores(response.data);
         console.log(scores);
       }
