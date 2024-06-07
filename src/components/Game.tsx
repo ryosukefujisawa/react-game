5bdb084f769c1653abbb6b63c14f655c6ca41c02
ryosuke fujisawa <ryosuke.fujisawa@goodsun-hd>
2024-06-07 11:00:44 +0900

    URL

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -21,7 +21,7 @@ const Game = () => {
         const fetchScores = async () => {
             try {
                 /* サーバーにHTTP GETリクエストを送信してスコアのリストを取得 */
-                const response = await axios.get<Score[]>('https://react-game.azurewebsites.net/scores');
+                const response = await axios.get<Score[]>('https://black-mud-04d1a9300.5.azurestaticapps.net/scores');
                 setScores(response.data);
                 console.log(scores);
             }
